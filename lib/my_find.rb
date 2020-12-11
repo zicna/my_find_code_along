ad854713a07353d36b5fd8e29f3c3fbbd01e64ec
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-11 12:20:35 -0600

    commit tree 1e28d54767fc876d90fb98aa715f607c78520cf2

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_find(collection)
-
+    #binding.pry
+    i = 0
+    coll_len = collection.length
+    while i < coll_len
+    #    if yield(collection[i])
+    #         return collection[i]
+    #     end
+    return collection[i] if yield(collection[i])
+        i += 1
+    end
 end
+range = (1..100).to_a
+puts my_find(range) {|item| item % 3 == 0 and item % 5 == 0}
